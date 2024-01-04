f4fc3b9d6048b859f72a4dedc5392f2bc23158a0
mei-sorter <106627409+mei-sorter@users.noreply.github.com>
2024-01-04 12:09:55 +0700

    Add files via upload

---
+++ pdjptg-songs/src/js/data/2024-01-03.js
@@ -0,0 +1,71 @@
+dataSetVersion = "2024-01-03"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Kato Cocoro",
+    img: "cocoro.jpg",
+    last_name: "Kato",
+    first_name: "Cocoro",
+  },
+  {
+    name: "Sakurai Miu",
+    img: "miu.jpg",
+    last_name: "Sakurai",
+    first_name: "Miu",
+  },
+  {
+    name: "Kasahara Momona",
+    img: "momona.jpg",
+    last_name: "Kasahara",
+    first_name: "Momona",
+  },
+  {
+    name: "Ishii Ran",
+    img: "ran.jpg",
+    last_name: "Ishii",
+    first_name: "Ran",
+  },
+  {
+    name: "Iida Shizuku",
+    img: "shizuku.jpg",
+    last_name: "Iida",
+    first_name: "Shizuku",
+  },
+  {
+    name: "Takami Ayane",
+    img: "ayane.jpg",
+    last_name: "Takami",
+    first_name: "Ayane",
+  },
+  {
+    name: "Shimizu Keiko",
+    img: "keiko.jpg",
+    last_name: "Shimizu",
+    first_name: "Keiko",
+  },
+  {
+    name: "Sasaki Kokona",
+    img: "kokona.jpg",
+    last_name: "Sasaki",
+    first_name: "Kokona",
+  },
+  {
+    name: "Murakami Rinon",
+    img: "rinon.jpg",
+    last_name: "Murakami",
+    first_name: "Rinon",
+  },
+  {
+    name: "Yamamoto Suzu",
+    img: "suzu.jpg",
+    last_name: "Yamamoto",
+    first_name: "Suzu",
+  },
+  {
+    name: "Ebihara Tsuzumi",
+    img: "tsuzumi.jpg",
+    last_name: "Ebihara",
+    first_name: "Tsuzumi",
+  }
+];
---
+++ pdjptg-songs/src/js/data/pdjptg-songs.js
@@ -0,0 +1,37 @@
+dataSetVersion = "pdjptg-songs"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "AtoZ",
+    img: "/pdjptg-songs/atoz.jpg",
+  },
+  {
+    name: "&ME",
+    img: "/pdjptg-songs/andMe.jpg",
+  },
+  {
+    name: "小悪魔 (Baddie)",
+    img: "/pdjptg-songs/baddie.jpg",
+  },
+  {
+    name: "CHOPPY CHOPPY",
+    img: "/pdjptg-songs/choppy.jpg",
+  },
+  {
+    name: "Fly Up So High",
+    img: "/pdjptg-songs/flyupsohigh.jpg",
+  },
+  {
+    name: "Popcorn",
+    img: "/pdjptg-songs/popcorn.jpg",
+  },
+  {
+    name: "想像以上 (Sozo Ijou)",
+    img: "/pdjptg-songs/sozoijou.jpg",
+  },
+  {
+    name: "TOXIC",
+    img: "/pdjptg-songs/toxic.jpg",
+  },
+];
