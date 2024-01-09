eb73bf0e2c7588dce45f862114ca03676d8188ab
mei-sorter <106627409+mei-sorter@users.noreply.github.com>
2024-01-09 15:35:47 +0700

    Add files via upload

---
+++ pdjptg-contestants/src/js/data/pdjptg-contestants.js
@@ -0,0 +1,500 @@
+dataSetVersion = "pdjptg-contestants"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Kasahara Momona",
+    img: "022_kasaharamomona.jpg",
+    rank: 20,
+  },
+  {
+    name: "Murakami Rinon",
+    img: "090_murakamirinon.jpg",
+    rank: 20,
+  },
+  {
+    name: "Takami Ayane",
+    img: "067_takamiayane.jpg",
+    rank: 20,
+  },
+  {
+    name: "Sakurai Miu",
+    img: "046_sakuraimiu.jpg",
+    rank: 20,
+  },
+  {
+    name: "Yamamoto Suzu",
+    img: "096_yamamotosuzu.jpg",
+    rank: 20,
+  },
+  {
+    name: "Sasaki Kokona",
+    img: "048_sasakikokona.jpg",
+    rank: 20,
+  },
+  {
+    name: "Iida Shizuku",
+    img: "009_iidashizuku.jpg",
+    rank: 20,
+  },
+  {
+    name: "Shimizu Keiko",
+    img: "054_shimizukeiko.jpg",
+    rank: 20,
+  },
+  {
+    name: "Ishii Ran",
+    img: "010_ishiiran.jpg",
+    rank: 20,
+  },
+  {
+    name: "Ebihara Tsuzumi",
+    img: "014_ebiharatsuzumi.jpg",
+    rank: 20,
+  },
+  {
+    name: "Kato Cocoro",
+    img: "026_katokokoro.jpg",
+    rank: 20,
+  },
+  {
+    name: "Sakaguchi Rino",
+    img: "044_sakaguchirino.jpg",
+    rank: 20,
+  },
+  {
+    name: "Aita Rin",
+    img: "001_aitarin.jpg",
+    rank: 20,
+  },
+  {
+    name: "Kato Kagura",
+    img: "025_katokagura.jpg",
+    rank: 20,
+  },
+  {
+    name: "Tanaka Yuuki",
+    img: "070_tanakayuuki.jpg",
+    rank: 20,
+  },
+  {
+    name: "Kenmotsu Nano",
+    img: "039_kenmotsunano.jpg",
+    rank: 20,
+  },
+  {
+    name: "Tanaka Koto",
+    img: "068_tanakakoto.jpg",
+    rank: 20,
+  },
+  {
+    name: "Kitazato Rio",
+    img: "034_kitazatorio.jpg",
+    rank: 20,
+  },
+  {
+    name: "Sakuraba Haruka",
+    img: "047_sakurabaharuka.jpg",
+    rank: 20,
+  },
+  {
+    name: "Takabatake Momoka",
+    img: "066_takabatakemomoka.jpg",
+    rank: 20,
+  },
+  {
+    name: "Saito Serina",
+    img: "043_saitoserina.jpg",
+    rank: 35,
+  },
+  {
+    name: "Aramaki Joa",
+    img: "005_aramakijoa.jpg",
+    rank: 35,
+  },
+  {
+    name: "Abe Nagomi",
+    img: "004_abenagomi.jpg",
+    rank: 35,
+  },
+  {
+    name: "Kamio Ayano",
+    img: "029_kamioayano.jpg",
+    rank: 35,
+  },
+  {
+    name: "Ando Yui",
+    img: "007_andoyui.jpg",
+    rank: 35,
+  },
+  {
+    name: "Takahashi Hina",
+    img: "065_takahashihina.jpg",
+    rank: 35,
+  },
+  {
+    name: "Sasaki Tsukushi",
+    img: "049_sasakitsukushi.jpg",
+    rank: 35,
+  },
+  {
+    name: "Kitazume Sakura",
+    img: "035_kitazumesakura.jpg",
+    rank: 35,
+  },
+  {
+    name: "Matsushita Miyu",
+    img: "088_matsushitamiyu.jpg",
+    rank: 35,
+  },
+  {
+    name: "Yoshida Hana",
+    img: "098_yoshidahana.jpg",
+    rank: 35,
+  },
+  {
+    name: "Hatta Mena",
+    img: "081_hattamena.jpg",
+    rank: 35,
+  },
+  {
+    name: "Sutani Yurara",
+    img: "060_sutaniyurara.jpg",
+    rank: 35,
+  },
+  {
+    name: "Kawabata Renka",
+    img: "031_kawabataranka.jpg",
+    rank: 35,
+  },
+  {
+    name: "Hidaka Hazuki",
+    img: "084_hidakahazuki.jpg",
+    rank: 35,
+  },
+  {
+    name: "Nakano Kokona",
+    img: "075_nakanokokona.jpg",
+    rank: 35,
+  },
+  {
+    name: "Sakata Kotone",
+    img: "045_sakatakotone.jpg",
+    rank: 50,
+  },
+  {
+    name: "Yoshida Ayano",
+    img: "097_yoshidaayano.jpg",
+    rank: 50,
+  },
+  {
+    name: "Koyama Mana",
+    img: "042_koyamamana.jpg",
+    rank: 50,
+  },
+  {
+    name: "Uchiyama Rin",
+    img: "013_uchiyamarin.jpg",
+    rank: 50,
+  },
+  {
+    name: "Otsubo Karen",
+    img: "017_otsubokaren.jpg",
+    rank: 50,
+  },
+  {
+    name: "Akiyama Ema",
+    img: "003_akiyamaema.jpg",
+    rank: 50,
+  },
+  {
+    name: "Takagi Mayu",
+    img: "064_takagimayu.jpg",
+    rank: 50,
+  },
+  {
+    name: "Mizukami Rimika",
+    img: "089_mizukamirimika.jpg",
+    rank: 50,
+  },
+  {
+    name: "Ando Chiharu",
+    img: "006_andochiharu.jpg",
+    rank: 50,
+  },
+  {
+    name: "Fujimoto Ayaka",
+    img: "085_fujimotoayaka.jpg",
+    rank: 50,
+  },
+  {
+    name: "Yamazaki Mitsuki",
+    img: "095_yamazakimitsuki.jpg",
+    rank: 50,
+  },
+  {
+    name: "Kanno Miyu",
+    img: "032_kannomiyu.jpg",
+    rank: 50,
+  },
+  {
+    name: "Nakamori Kotone",
+    img: "078_nakamorikotone.jpg",
+    rank: 50,
+  },
+  {
+    name: "Sudo Sakura",
+    img: "061_sudosakura.jpg",
+    rank: 50,
+  },
+  {
+    name: "Nakamori Mikoto",
+    img: "079_nakamorimikoto.jpg",
+    rank: 50,
+  },
+  {
+    name: "Yamaguchi Misaki",
+    img: "094_yamaguchimisaki.jpg",
+    rank: 96
+  },
+  {
+    name: "Mogi Shion",
+    img: "091_mogishion.jpg",
+    rank: 96
+  },
+  {
+    name: "Oda Aruha",
+    img: "020_odaaruha.jpg",
+    rank: 96
+  },
+  {
+    name: "Watanabe Miyu",
+    img: "100_watanabemiyu.jpg",
+    rank: 96
+  },
+  {
+    name: "Kurokawa Honoka",
+    img: "038_kurokawahonoka.jpg",
+    rank: 96
+  },
+  {
+    name: "Oikawa Rio",
+    img: "015_oikawario.jpg",
+    rank: 96
+  },
+  {
+    name: "Suzuki Rena",
+    img: "059_suzukirena.jpg",
+    rank: 96
+  },
+  {
+    name: "Kamada Moe",
+    img: "028_kamadamoe.jpg",
+    rank: 96
+  },
+  {
+    name: "Kurihara Kano",
+    img: "037_kuriharakano.jpg",
+    rank: 96
+  },
+  {
+    name: "Bando Fuka",
+    img: "083_bandofuka.jpg",
+    rank: 96
+  },
+  {
+    name: "Kikukawa Aki",
+    img: "033_kikukawaaki.jpg",
+    rank: 96
+  },
+  {
+    name: "Hamasaki Aiko",
+    img: "082_hamasakiaiko.jpg",
+    rank: 96
+  },
+  {
+    name: "Ueki Mimi",
+    img: "012_uekimimi.jpg",
+    rank: 96
+  },
+  {
+    name: "Tabata Nana",
+    img: "073_tabatanana.jpg",
+    rank: 96
+  },
+  {
+    name: "Hosoi Ayaka",
+    img: "087_hosoiayaka.jpg",
+    rank: 96
+  },
+  {
+    name: "Tanabe Karin",
+    img: "071_tanabekarin.jpg",
+    rank: 96
+  },
+  {
+    name: "Okamura Nana",
+    img: "019_okamuranana.jpg",
+    rank: 96
+  },
+  {
+    name: "Shiromaru Maho",
+    img: "056_shiromarumaho.jpg",
+    rank: 96
+  },
+  {
+    name: "Okabe Nonoka",
+    img: "018_okabenonoka.jpg",
+    rank: 96
+  },
+  {
+    name: "Tanaka Hana",
+    img: "069_tanakahana.jpg",
+    rank: 96
+  },
+  {
+    name: "Iyota Hana",
+    img: "011_iyotahana.jpg",
+    rank: 96
+  },
+  {
+    name: "Wakimoto Mihaya",
+    img: "099_wakimotomihaya.jpg",
+    rank: 96
+  },
+  {
+    name: "Oyanagi Emi",
+    img: "021_oyanagiemi.jpg",
+    rank: 96
+  },
+  {
+    name: "Moro Anon",
+    img: "093_moroanon.jpg",
+    rank: 96
+  },
+  {
+    name: "Shibuya Mei",
+    img: "053_shibuyamei.jpg",
+    rank: 96
+  },
+  {
+    name: "Nakamura Aoi",
+    img: "076_nakamuraaoi.jpg",
+    rank: 96
+  },
+  {
+    name: "Kawagishi Runa",
+    img: "030_kawagishiruna.jpg",
+    rank: 96
+  },
+  {
+    name: "Nakayama Honoka",
+    img: "080_nakayamahonoka.jpg",
+    rank: 96
+  },
+  {
+    name: "Sato Ameli",
+    img: "050_satoameli.jpg",
+    rank: 96
+  },
+  {
+    name: "Kato Konomi",
+    img: "027_katokonomi.jpg",
+    rank: 96
+  },
+  {
+    name: "Aoki Yuka",
+    img: "002_aokiyuka.jpg",
+    rank: 96
+  },
+  {
+    name: "Kataoka Rio",
+    img: "023_kataokario.jpg",
+    rank: 96
+  },
+  {
+    name: "Sekiguchi Rikako",
+    img: "062_sekiguchirikako.jpg",
+    rank: 96
+  },
+  {
+    name: "Kino Riko",
+    img: "036_kinoriko.jpg",
+    rank: 96
+  },
+  {
+    name: "Shinzawa Mika",
+    img: "057_shinzawamika.jpg",
+    rank: 96
+  },
+  {
+    name: "Tabuchi Miu",
+    img: "074_tabuchimiu.jpg",
+    rank: 96
+  },
+  {
+    name: "Motohashi Meika",
+    img: "092_motohashimeika.jpg",
+    rank: 96
+  },
+  {
+    name: "Sugai Natsuho",
+    img: "058_sugainatsuho.jpg",
+    rank: 96
+  },
+  {
+    name: "Nakamura Riro",
+    img: "077_nakamurariro.jpg",
+    rank: 96
+  },
+  {
+    name: "Ota Sara",
+    img: "016_otasara.jpg",
+    rank: 96
+  },
+  {
+    name: "Kato Airi",
+    img: "024_katoairi.jpg",
+    rank: 96
+  },
+  {
+    name: "Sano Jueri",
+    img: "051_sanojueri.jpg",
+    rank: 96
+  },
+  {
+    name: "Furuhashi Sayaka",
+    img: "086_furuhashisayaka.jpg",
+    rank: 96
+  },
+  {
+    name: "Tani Seia",
+    img: "072_taniseia.jpg",
+    rank: 96
+  },
+  {
+    name: "Kobayashi Sae",
+    img: "041_kobayashisae.jpg",
+    rank: 96
+  },
+  {
+    name: "Shibagaki Arisa",
+    img: "052_shibagakiarisa.jpg",
+    rank: 96
+  }
+];
+
+dataSet[dataSetVersion].options = [
+  {
+      name: 'Filter by Ranking',
+      key: 'rank',
+      tooltip: 'Check this to restrict to certain ranking groups',
+      checked: false,
+      sub: [
+          { name: 'Top 20', key: 'top20'},
+          { name: 'Top 35', key: 'top35'},
+          { name: 'Top 50', key: 'top50'},
+          { name: 'Top 96', key: 'top96'},
+      ]
+  },
+]
